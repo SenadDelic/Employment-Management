92b9109658d887f06580ae2a68d426442604e3c5
Senad <dellic.senad@gmail.com>
2020-12-11 18:31:04 +0100

    Working on update/delete functions

--- src/main/java/com/management/controller/EmployeeController.java
+++ src/main/java/com/management/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -34,10 +35,12 @@ public class EmployeeController {
         return "addNewEmployee";
     }
 
-    @GetMapping("/updateEmployee")
-    public String updateEmployee() {
+    @GetMapping("/updateEmployee/{id}")
+    public String updateEmployee(@PathVariable(value = "id") long id, Model model) {
+        Employee employee = employeeService.getEmployeeById(id);
+        model.addAttribute("employee", employee);
 
-        return "something";
+        return "updateEmployee";
     }
 
     @PostMapping("/saveEmployee")
